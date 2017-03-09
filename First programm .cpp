15ac60a82d4f48500b4ec275489b9ff62ed93527
Romanov Sergey <rsv.981@gmail.com>
2017-03-09 14:00:21 +0300

    test fork and pull request and utf-8 text

--- First programm .cpp
+++ First programm .cpp
@@ -1,7 +1,26 @@
 //First programm
-#include<iostream>
+/*
+ * Test gitHub functionality
+ * Autor: Romanov Victor
+ * folowers: Romanov Sergey
+ * (russian UTF-8 text: Программа печатает "Здравствуй мир!")
+ */
+#include <iostream>
+#include <unistd.h> // for sleep() function (not standart ANSI С)
+
 using namespace std;
+
 int main()
 {
   setlocale(LC_ALL,"rus");
   
+  string name;
+  
+  cout << "Hello world!" << endl;
+  cout << "Enter your name: ";
+  cin  >> name;
+  cout << endl;
+  cout << "Hello " << name << endl;
+  sleep(2);
+  
+}
